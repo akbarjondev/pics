8331c9153867187a56b460eed47a08bbe9362c40
Akbarjon <tojiyev.akbarjon@gmail.com>
2022-02-18 01:19:03 +0500

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import unsplash from '../api/unsplash';
+import ImageList from './ImageList';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-  <div className='ui container'>
-    <SearchBar />
-  </div>
-  );
+class App extends React.Component {
+  state = { images: [] }
+
+  onFormSubmit = async (term) => {
+    const response = await unsplash.get('/search/photos', {
+      params: {
+        query: term
+      }
+    });
+
+    this.setState({images: response.data.results});
+  }
+  
+  render() {
+    return (
+    <div className='ui container'>
+      <SearchBar onSubmit={this.onFormSubmit} nimadir='data' />
+      <ImageList images={this.state.images} />
+    </div>
+    );
+  }
 }
 
 export default App;
